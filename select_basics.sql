e1a29180cb06c4a2a38ba51b1af7a8865bca6e6d
Yanick <tyankee15@gmail.com>
2020-09-29 15:34:55 -0300

    solution to zoo tutorial numer 1

--- select_basics.sql
+++ select_basics.sql
@@ -1,4 +1,4 @@
--- SELECT basics
+-- 0 SELECT basics
 
 -- 1. Introducing the world table of countries
 
@@ -24,7 +24,10 @@ WHERE name IN ('Sweden', 'Norway', 'Denmark');
 
 -- 3. Just the right size
 
--- Which countries are not too small and not too big? BETWEEN allows range checking (range specified is inclusive of boundary values). The example below shows countries with an area of 250,000-300,000 sq. km. Modify it to show the country and the area for countries with an area between 200,000 and 250,000.
+-- Which countries are not too small and not too big? BETWEEN allows range checking 
+-- (range specified is inclusive of boundary values). The example below shows countries 
+-- with an area of 250,000-300,000 sq. km. Modify it to show the country and the area 
+-- for countries with an area between 200,000 and 250,000.
 SELECT name, area
 FROM world
 WHERE area BETWEEN 250000 AND 300000
