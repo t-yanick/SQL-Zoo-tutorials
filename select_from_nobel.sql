8b491ab9f11327bffc3df450dabf81c4fba43d5c
Yanick <tyankee15@gmail.com>
2020-09-30 15:34:15 -0300

    part 2 solution for select from nobel tutorial

--- select_from_nobel.sql
+++ select_from_nobel.sql
@@ -42,3 +42,88 @@ SELECT winner
 FROM nobel
 WHERE subject = 'Peace'
     AND yr >= 2000
+
+-- 5. Show all details (yr, subject, winner) of the Literature prize winners for 1980 to 1989 inclusive. 
+
+SELECT yr, subject, winner
+FROM nobel
+WHERE yr >= 1980
+    AND yr <= 1989
+    AND subject = 'Literature'
+
+-- Show all details of the presidential winners:
+
+-- Theodore Roosevelt
+-- Woodrow Wilson
+-- Jimmy Carter
+-- Barack Obama
+
+SELECT *
+FROM nobel
+WHERE winner IN ('Theodore Roosevelt',
+                  'Woodrow Wilson',
+                  'Jimmy Carter',
+                   'Barack Obama')
+
+-- 7. Show the winners with first name John 
+
+SELECT winner
+FROM nobel
+WHERE winner LIKE ('John%')
+
+-- 8. Show the year, subject, and name of Physics winners for 1980 together with the Chemistry winners for 1984.
+
+SELECT yr, subject, winner
+FROM nobel
+WHERE subject = 'Physics'
+    AND yr = 1980
+    OR subject = 'Chemistry'
+    AND yr = 1984
+
+-- 9. Show the year, subject, and name of winners for 1980 excluding Chemistry and Medicine
+
+SELECT yr, subject, winner
+FROM nobel
+WHERE yr = 1980 AND subject NOT IN ('Chemistry', 'Medicine')
+
+-- 10. Show year, subject, and name of people who won a 'Medicine' prize in an early year (before 1910, not including 1910) together with winners of a 'Literature' prize in a later year (after 2004, including 2004) 
+
+SELECT *
+FROM nobel
+WHERE (subject  = 'Medicine' AND yr < 1910)
+    OR (subject = 'Literature' AND yr >= 2004)
+
+-- 11. Find all details of the prize won by PETER GRÜNBERG
+--Non-ASCII characters
+-- The u in his name has an umlaut. You may find this link useful https://en.wikipedia.org/wiki/%C3%9C#Keyboarding    
+
+SELECT *
+FROM nobel
+WHERE winner LIKE 'peter gr%nberg'
+
+-- 12. Find all details of the prize won by EUGENE O'NEILL
+-- Escaping single quotes
+-- You can't put a single quote in a quote string directly. You can use two single quotes within a quoted string.
+
+SELECT *
+FROM nobel
+WHERE winner = 'EUGENE O''NEILL'
+
+-- 13. Knights in order
+
+--List the winners, year and subject where the winner starts with Sir. Show the the most recent first, then by name order.    
+
+SELECT winner, yr, subject
+FROM nobel
+WHERE winner LIKE 'Sir%'
+
+-- 14. The expression subject IN ('Chemistry','Physics') can be used as a value - it will be 0 or 1.
+--Show the 1984 winners and subject ordered by subject and winner name; but list Chemistry and Physics last.
+
+SELECT winner, subject
+FROM nobel
+WHERE yr=1984
+ORDER BY 
+    CASE WHEN subject IN ('Physics','Chemistry')
+    THEN 1 ELSE 0 END,
+    subject, winner
