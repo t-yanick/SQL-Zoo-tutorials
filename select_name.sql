b3b99fea897136282e2d59e83f6d325859f5e426
Yanick <tyankee15@gmail.com>
2020-09-30 03:39:13 -0300

    solution to select from world zoo tutorials

--- select_name.sql
+++ select_name.sql
@@ -1,148 +1,155 @@
 -- 1 SELECT name
 
--- 1. Introduction
+-- 1. 
+-- You can use WHERE name LIKE 'B%' to find the countries that start with "B".
 
--- Observe the result of running this SQL command to show the name, 
--- continent and population of all countries.
-SELECT name, continent, population
-FROM world
+-- The % is a wild-card it can match any characters
 
--- 2. Large Countries
-
--- How to use WHERE to filter records. Show the name for the countries 
--- that have a population of at least 200 million. 200 million is 200000000, 
--- there are eight zeros.
+-- Find the country that start with Y
 SELECT name
-FROM world
-WHERE population = 64105700
-
-SELECT name
-FROM world
-WHERE population >= 200000000
-
--- 3. Per capita GDP
-
--- Give the name and the per capita GDP for those countries with a population of at least 200 million. 
--- HELP:How to calculate per capita GDP
--- per capita GDP is the GDP divided by the population GDP/population
-
-SELECT name, gdp/population
-FROM world
-WHERE population >= 200000000
-
--- 4. South America In millions
-
--- Show the name and population in millions for the countries of the continent 'South America'. Divide the population by 1000000 to get population in millions. 
-
-SELECT name, population/1000000
-FROM world
-WHERE continent = 'South America'
-
--- 5. France, Germany, Italy
-
--- Show the name and population for France, Germany, Italy 
-
-SELECT name, population
 FROM world
 WHERE name 
-IN ('France', 'Germany', 'Italy')
+LIKE 'F%'
 
--- 6. United
+SELECT name
+FROM world
+WHERE name LIKE 'Y%'
 
--- Show the countries which have a name that includes the word 'United' 
+-- 2.
+-- Find the countries that end with y
+SELECT name
+FROM world
+WHERE name 
+LIKE 'T%'
 
 SELECT name
 FROM world
 WHERE name 
-LIKE '%United%'
+LIKE '%Y'
 
--- 7. Two ways to be big
-
--- Two ways to be big: A country is big if it has an area of more than 3 million sq km or it has a population of more than 250 million.
--- Show the countries that are big by area or big by population. Show name, population and area.
-
-SELECT name, population, area
+-- 3.
+-- Luxembourg has an x - so does one other country. List them both.
+-- Find the countries that contain the letter x
+SELECT name
 FROM world
-WHERE area > 3000000 OR population > 250000000
-
--- 8. One or the other (but not both)
-
--- Exclusive OR (XOR). Show the countries that are big by area (more than 3 million) or big by population (more than 250 million) but not both. Show name, population and area.
-
--- Australia has a big area but a small population, it should be included.
--- Indonesia has a big population but a small area, it should be included.
--- China has a big population and big area, it should be excluded.
--- United Kingdom has a small population and a small area, it should be excluded.
-
-SELECT name, population, area
-FROM world
-WHERE area > 3000000
-XOR population > 250000000
-
--- 9. Rounding
-
--- Show the name and population in millions and the GDP in billions for the countries of the continent 'South America'. Use the ROUND function to show the values to two decimal places.
--- For South America show population in millions and GDP in billions both to 2 decimal places.
--- Millions and billions
--- Divide by 1000000 (6 zeros) for millions. Divide by 1000000000 (9 zeros) for billions.
-
-SELECT name, ROUND(population/1000000,2), ROUND(gdp/1000000000,2)
-FROM world
-WHERE continent = 'South America'
-
--- 10. Trillion dollar economies
-
--- Show the name and per-capita GDP for those countries with a GDP of at least one trillion (1000000000000; that is 12 zeros). Round this value to the nearest 1000.
--- Show per-capita GDP for the trillion dollar countries to the nearest $1000.
-
-SELECT name, ROUND(gdp/population,-3)
-FROM world
-WHERE gdp >= 1000000000000
-
--- 11. Name and capital have the same length
-
--- Greece has capital Athens.
--- Each of the strings 'Greece', and 'Athens' has 6 characters.
--- Show the name and capital where the name and the capital have the same number of characters.
---You can use the LENGTH function to find the number of characters in a string
-
-SELECT name, LENGTH(name),
-    continent, LENGTH(continent),
-    capital, LENGTH(capital)
-FROM world
-WHERE name LIKE 'G%'
-
-SELECT name, capital
-FROM world
-WHERE LENGTH(name) = LENGTH(capital)
-
--- Matching name and capital
-
--- The capital of Sweden is Stockholm. Both words start with the letter 'S'.
--- Show the name and the capital where the first letters of each match. Don't include countries where the name and the capital are the same word.
-
--- You can use the function LEFT to isolate the first character.
---You can use <> as the NOT EQUALS operator.
-
-SELECT name, capital
-FROM world
-WHERE LEFT(name, 1) = LEFT(capital, 1)
-    AND name <> capital;
-
--- 13. All the vowels
-
--- Equatorial Guinea and Dominican Republic have all of the vowels (a e i o u) in the name. They don't count because they have more than one word in the name.
-
--- Find the country that has all the vowels and no spaces in its name.
-
--- You can use the phrase name NOT LIKE '%a%' to exclude characters from your results.
--- The query shown misses countries like Bahamas and Belarus because they contain at least one 'a'     
+WHERE name 
+LIKE 'T%'
 
 SELECT name
 FROM world
-WHERE name LIKE '%a%'
-    AND name LIKE '%e%'
-    AND name LIKE '%i%'
-    AND name LIKE '%o%'
-    AND name LIKE '%u%'
-    AND name NOT LIKE '% %';
+WHERE name 
+LIKE '%X%'
+
+--4. 
+-- Iceland, Switzerland end with land - but are there others?
+-- Find the countries that end with land
+
+SELECT name
+FROM world
+WHERE name 
+LIKE '%land'
+
+-- 5. 
+-- Columbia starts with a C and ends with ia - there are two more like this.
+-- Find the countries that start with C and end with ia
+
+SELECT name
+FROM world
+WHERE name 
+LIKE 'C%'
+    AND name 
+LIKE '%ia'
+
+-- 6.
+-- Greece has a double e - who has a double o?
+-- Find the country that has oo in the name
+SELECT name
+FROM world
+WHERE name 
+LIKE '%ee%'
+
+SELECT name
+FROM world
+WHERE name 
+LIKE '%oo%'
+
+-- 7.
+--Bahamas has three a - who else?
+-- Find the countries that have three or more a in the name
+
+SELECT name
+FROM world
+WHERE name LIKE '%a%a%a%'
+
+-- 8.
+-- India and Angola have an n as the second character. You can use the underscore as a single character wildcard.
+
+SELECT name
+FROM world
+WHERE name LIKE '_n%'
+ORDER BY name
+
+-- Find the countries that have "t" as the second character.
+
+SELECT name
+FROM world
+WHERE name LIKE '_t%'
+ORDER BY name
+
+-- 9.
+-- Lesotho and Moldova both have two o characters separated by two other characters.
+-- Find the countries that have two "o" characters separated by two others.
+
+SELECT name
+FROM world
+WHERE name LIKE '%o__o%'
+
+-- 10.
+-- Cuba and Togo have four characters names.
+-- Find the countries that have exactly four characters.
+
+SELECT name
+FROM world
+WHERE LENGTH(name) = 4
+
+-- 11.
+-- The capital of Luxembourg is Luxembourg. Show all the countries where the capital is the same as the name of the country
+-- Find the country where the name is the capital city.
+
+SELECT name
+FROM world
+WHERE name = capital
+
+-- 12. 
+-- The capital of Mexico is Mexico City. Show all the countries where the capital has the country together with the word "City".
+-- Find the country where the capital is the country plus "City".
+
+SELECT name
+FROM world
+WHERE capital LIKE '%City'
+
+-- 13.
+-- Find the capital and the name where the capital includes the name of the country.
+
+SELECT capital, name
+FROM world
+WHERE capital LIKE CONCAT('%', name, '%')
+
+-- 14.
+-- Find the capital and the name where the capital is an extension of name of the country.
+-- You should include Mexico City as it is longer than Mexico. You should not include Luxembourg as the capital is the same as the country. 
+
+SELECT capital, name
+FROM world
+WHERE capital LIKE CONCAT('%', name, '%')
+    AND capital > name
+
+-- 15. 
+-- For Monaco-Ville the name is Monaco and the extension is -Ville.
+-- Show the name and the extension where the capital is an extension of name of the country.
+-- You can use the SQL function REPLACE. 
+
+SELECT name, REPLACE(capital, name, '')
+FROM world
+WHERE capital LIKE CONCAT('%', name, '%')
+    AND capital > name 
